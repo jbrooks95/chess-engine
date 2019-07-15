107a48dff3fb33124dade731e02f646029e8e7fc
James Brooks <jamesbrooks1995@gmail.com>
2019-07-15 16:03:09 -0600

    update make and engine for basic eval function

--- src/bitboard.h
+++ src/bitboard.h
@@ -1,3 +1,8 @@
+#ifndef BITBOARD_H
+#define BITBOARD_H
+
 #include <stdint.h>
 
 typedef uint64_t bitboard;
+
+#endif
--- src/engine.c
+++ src/engine.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <board.h>
+#include <evaluate.h>
 
 void main()
 {
     board* b = init_board();
     print_board(b);
+    int eval = evaluate(b);
+    printf("evaluation = %d", eval);
 }
